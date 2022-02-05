1c016f7146314b93032f8f4974ab2b83af8b8fad
Paul Ritter <ritterpw@dukes.jmu.edu>
2022-02-05 16:01:50 -0500

    updating createSendToken

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,19 +11,16 @@ const signToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
 
-  const cookieOptions = {
+  res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-  };
-
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-  res.cookie('jwt', token, cookieOptions);
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  });
 
   user.password = undefined;
 
@@ -49,7 +46,7 @@ exports.signUp = catchAsync(async (req, res, next) => {
   console.log(url);
   await new Email(newUser, url).sendWelcome();
 
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -64,7 +61,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('incorrect email or password', 400));
   }
 
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.logout = (req, res) => {
@@ -214,7 +211,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   //update changed password at
   //log the user in, send JWT
 
-  createSendToken(user, 201, res);
+  createSendToken(user, 201, req, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -232,5 +229,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   await user.save();
 
   //update user
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
